757ea24beb3b08c8d33ae5180ef61757570c0b4a
Yaiza Aragonés Soria <yaiza.aragonessoria@gmail.com>
2023-02-02 11:41:17 +0100

    balances of home members computed

--- backend/expenses/admin.py
+++ backend/expenses/admin.py
@@ -4,6 +4,7 @@ from expenses.models import Expense
 
 class ExpenseAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'amount', 'creator', 'payer', 'show_shared_with')
+
     def show_shared_with(self, obj):
         return ", ".join([user.email for user in obj.shared_with.all()])
 
--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -4,46 +4,57 @@ from expenses.models import Expense
 from users.models import User
 
 
+def compute_member_balance(id_member, id_home):
+    home_expenses = Expense.objects.filter(creator_id__home=id_home)
+
+    member_balance = 0
+    for expense in home_expenses:
+        id_expense = expense.id
+        shared_with = Expense.objects.filter(id=id_expense).values('shared_with')
+        num_members_sharing = len(shared_with)
+        amount = Expense.objects.filter(id=id_expense).values('amount')[0].get('amount', None)
+        payer = Expense.objects.filter(id=id_expense).values('payer')[0].get('payer', None)
+
+        # is member involved in this expense?
+        for member in shared_with:
+            if id_member == member.get('shared_with', None):
+                is_member_in_expense = 1
+                break
+            else:
+                is_member_in_expense = 0
+
+        to_pay = is_member_in_expense * amount / num_members_sharing
+
+        # has member paid?
+        if id_member == payer:
+            is_member_payer = 1
+        else:
+            is_member_payer = 0
+
+        paid = is_member_payer * amount
+
+        member_balance = member_balance + (paid - to_pay)
+
+    return member_balance
+
+
+
 class UserSerializer(serializers.ModelSerializer):
     balance = serializers.SerializerMethodField()
 
     def get_balance(self, user):
-        id_user = user.id
+        # user=logged-in user, memeber=users sharing logged-in-user (including logged-in user)
         id_home = user.home
         home_members = User.objects.filter(home=id_home)
-        home_id_expenses = Expense.objects.filter(creator_id__home=id_home).values('id')
-        num_expenses = len(home_id_expenses)
 
-        total = 0
-        for i, id_expense in enumerate(home_id_expenses):
-            id = id_expense['id']
-            shared_with = Expense.objects.filter(id=id).values('shared_with')
-            num_members_sharing = len(shared_with)
-            amount = Expense.objects.filter(id=id).values('amount')[0].get('amount', None)
-            payer = Expense.objects.filter(id=id).values('payer')[0].get('payer', None)
-            # amount = amount_query[0].get('amount', None)
+        # compute balance of all members
+        member_balances = []
+        for member in home_members:
+            id_member = member.id
+            member_balance = compute_member_balance(id_member, id_home)
+            member_balances.append([member.id, member_balance])
 
-            # is user involved in this expense?
-            for member in shared_with:
-                if id_user == member.get('shared_with', None):
-                    is_user_in_expense = 1
-                    break
-                else:
-                    is_user_in_expense = 0
-
-            to_pay = is_user_in_expense * amount / num_members_sharing
-
-            # has user paid?
-            if id_user == payer:
-                is_user_payer = 1
-            else:
-                is_user_payer = 0
-
-            paid = is_user_payer * amount
-
-            total = total + (paid - to_pay)
-
-        return total
+        return member_balances
 
     class Meta:
         model = User
