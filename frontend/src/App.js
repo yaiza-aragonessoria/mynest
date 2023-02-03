148c3112d9860399d5a2a7fd8713ca80927bf594
Yaiza Aragonés Soria <yaiza.aragonessoria@gmail.com>
2023-02-03 21:24:23 +0100

    login, logout, navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,19 @@ import {Route, Routes} from 'react-router-dom';
 
 import './App.css';
 import Shoppinglist from './pages/ShoppingList/ShoppingList';
+import Login from "./pages/Login/Login";
+import Header from "./components/Header/Header";
+import Home from "./pages/Home/Home";
+
 
 function App() {
     return (
         <div className="App">
+            <Header/>
             <Routes>
-                {/*<Route path='/' element={</>}/>*/}
+                <Route path='/' element={<Home/>}/>
                 <Route path="/shoppinglist" element={<Shoppinglist/>}/>
+                <Route path="/login" element={<Login/>}/>
             </Routes>
 
         </div>);
---
+++ frontend/src/pages/Home/Home.js
@@ -0,0 +1,12 @@
+import React, {useEffect, useState} from "react";
+const Home = () => {
+
+    return (
+        <div>
+            Home
+        </div>
+    );
+};
+
+
+export default Home;
