8b77e956351beec525380f8865180ed09d00c97a
Julia Reva <rse.jreva@gmail.com>
2023-02-05 23:19:29 +0100

    Add sorting for favorites. Added backend for like and delete

--- frontend/src/components/FavouriteItems/FavouriteItems.jsx
+++ frontend/src/components/FavouriteItems/FavouriteItems.jsx
@@ -2,12 +2,36 @@ import React, { useState } from "react";
 import FavouriteItems_popup from "./FavouriteItems_popup";
 
 const FavouriteItems = ({ tobuyItem, updateCartStatus, setTobuyItem }) => {
+  const [showPopup, setShowPopup] = useState(false);
 
-const [showPopup, setShowPopup] = useState(false);
+  const handlePopup = (e) => {
+    setShowPopup(true);
+  };
 
-const handlePopup = (e) => {
-  setShowPopup(true); 
-}
+  const sortFunction = (a, b) => {
+    if ((a.favorite && b.favorite) || (!a.favorite && !b.favorite)) {
+      // sorting by oldest (among favorites or among non-favorites)
+      if (a.updated < b.updated) {
+        return -1;
+      } else if (a.updated > b.updated) {
+        return 1;
+      }
+
+      if (a.name.toLowerCase() < b.name.toLowerCase()) {
+        return -1;
+      } else if (a.name.toLowerCase() > b.name.toLowerCase()) {
+        return 1;
+      }
+      return 0;
+    } else {
+      // sorting for favorite vs non-favorite
+      if (a.favorite && !b.favorite) {
+        return -1;
+      } else {
+        return 1;
+      }
+    }
+  };
 
   return (
     <div>
@@ -16,25 +40,29 @@ const handlePopup = (e) => {
           .filter((item) => {
             return item.status === "BO";
           })
+          .slice()
+          .sort(sortFunction)
           .map((item, index) => {
             return (
-              <span
-                onClick={() => {
-                  updateCartStatus(item.id, "TB");
-                }}
-                key={item.id}
-              >
-                {item.name}
-              </span>
+              <div key={item.id}>
+                <span
+                  onClick={() => {
+                    updateCartStatus(item.id, "TB");
+                  }}
+                >
+                  {item.name}
+                </span>
+                <br />
+              </div>
             );
           })}
       </div>
       <button onClick={handlePopup}>Edit</button>
-      <FavouriteItems_popup 
-      showPopup={showPopup}
-      setShowPopup={setShowPopup}
-      tobuyItem={tobuyItem}
-      setTobuyItem={setTobuyItem}
+      <FavouriteItems_popup
+        showPopup={showPopup}
+        setShowPopup={setShowPopup}
+        tobuyItem={tobuyItem}
+        setTobuyItem={setTobuyItem}
       />
     </div>
   );
