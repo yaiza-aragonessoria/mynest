200c4b86c3e82251839799a27ceafffe7d801467
Laura Balestra <laurabalestra@Lauras-MacBook-Air.local>
2023-02-07 16:17:31 +0100

    initial home frontend

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -1,5 +1,23 @@
 import React, {useEffect, useState} from "react";
+import axios from "axios";
 const Home = () => {
+    const token = localStorage.getItem("access");
+
+    const config = {
+        headers: { Authorization: `Bearer ${token}` },
+    };
+
+    const [, setTobuyItem] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const backendData = await axios.get(
+                "https://mynest.propulsion-learn.ch/backend/api/home/", config
+            );
+            console.log(backendData);
+        };
+        fetchData();
+    }, []);
 
     return (
         <div>
