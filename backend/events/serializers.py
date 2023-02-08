28904a49f460d7af9463814790c13f0721682206
Yaiza Aragonés Soria <yaiza.aragonessoria@gmail.com>
2023-02-08 10:44:02 +0100

    avatar in user admin

--- backend/events/serializers.py
+++ backend/events/serializers.py
@@ -14,8 +14,8 @@ class EventSerializer(serializers.ModelSerializer):
         fields = '__all__'
         read_only_fields = ['creator']
 
-    def to_representation(self, expense):
-        representation = super().to_representation(expense)
-        participants_queryset = User.objects.filter(id__in=representation['participants'])
-        representation['participants'] = UserSerializer(participants_queryset, many=True).data
-        return representation
+    # def to_representation(self, expense):
+    #     representation = super().to_representation(expense)
+    #     participants_queryset = User.objects.filter(id__in=representation['participants'])
+    #     representation['participants'] = UserSerializer(participants_queryset, many=True).data
+    #     return representation
