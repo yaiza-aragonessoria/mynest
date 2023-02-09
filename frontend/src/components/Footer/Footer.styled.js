045e9a15b747cba89d408eef1dcc67f8a42cfcf8
Julia Reva <rse.jreva@gmail.com>
2023-02-09 14:09:37 +0100

    footer created and styled

---
+++ frontend/src/components/Footer/Footer.styled.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const FooterLayout = styled.footer`
+height: var(--footerheight);
+width: 100%;
+background-color: #B5B5C3;
+font-style: normal;
+font-weight: 500;
+font-size: 12.35px;
+line-height: 19px;
+color: #fff;
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+
+`
