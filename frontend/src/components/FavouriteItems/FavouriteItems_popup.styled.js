cc899598b63d7673044ecbf151edba03f6b17bcd
Julia Reva <rse.jreva@gmail.com>
2023-02-10 12:51:42 +0100

    fav items popup

--- frontend/src/components/FavouriteItems/FavouriteItems_popup.styled.js
+++ frontend/src/components/FavouriteItems/FavouriteItems_popup.styled.js
@@ -2,7 +2,9 @@ import styled from "styled-components";
 
 export const PopupBg = styled.div`
 width: 100%;
-height: 100%;
+min-height: 100%;
+top: 0;
+left: 0;
 z-index: 3;
 position: fixed;
 background: rgb(248,245,255);
