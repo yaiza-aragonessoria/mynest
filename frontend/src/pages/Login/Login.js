abbffa822ac4d8ffa194f17a6897b1e12b83527e
Julia Reva <rse.jreva@gmail.com>
2023-02-12 20:42:40 +0100

    login page bg added

--- frontend/src/pages/Login/Login.js
+++ frontend/src/pages/Login/Login.js
@@ -1,98 +1,127 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { useSelector } from 'react-redux';
+import { useSelector } from "react-redux";
 import axios from "axios";
-import {Link, useNavigate} from "react-router-dom";
-import {setAuth} from "../../features/slices/authSlice";
-import {ErrorMessage, FormFields, LoginTitle, LoginWrapper, SignIn, Wrapper} from "./Login.styled";
+import { Link, useNavigate } from "react-router-dom";
+import { setAuth } from "../../features/slices/authSlice";
+import {
+  ErrorMessage,
+  FormFields,
+  LoginTitle,
+  LoginWrapper,
+  SignIn,
+  Wrapper,
+} from "./Login.styled";
 
 
 const Login = () => {
-  const [newEmail, setNewEmail] = useState('');
-  const [newPassword, setNewPassword] = useState('');
-  const [newWarning, setNewWarning] = useState('');
+  const [newEmail, setNewEmail] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [newWarning, setNewWarning] = useState("");
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
   const auth = useSelector((state) => state.auth.data);
 
-
-  const handleChange = e => {
-    if(e.target.name === "email") {
+  const handleChange = (e) => {
+    if (e.target.name === "email") {
       setNewEmail(e.target.value);
     } else {
       setNewPassword(e.target.value);
     }
-  }
+  };
 
-  const handleSubmit = e => {
+  const handleSubmit = (e) => {
     e.preventDefault();
 
     const data = {
       email: newEmail,
       password: newPassword,
     };
-    axios.post("https://mynest.propulsion-learn.ch/backend/api/auth/token/", data).then(res => {
-      setNewWarning("");
-      const authData = {
-        email: newEmail,
-        access: res.data.access,
+    axios
+      .post("https://mynest.propulsion-learn.ch/backend/api/auth/token/", data)
+      .then((res) => {
+        setNewWarning("");
+        const authData = {
+          email: newEmail,
+          access: res.data.access,
+        };
 
-      }
+        localStorage.setItem("access", authData.access);
+        localStorage.setItem("email", authData.email);
 
-      localStorage.setItem('access', authData.access);
-      localStorage.setItem('email', authData.email);
+        dispatch(setAuth(authData));
 
-      dispatch(setAuth(authData));
-
-      navigate("/");
-
-    }).catch(error => {
-      console.log(error)
-      setNewWarning(error.response.data.detail);
-    });
-  }
+        navigate("/");
+      })
+      .catch((error) => {
+        console.log(error);
+        setNewWarning(error.response.data.detail);
+      });
+  };
 
   useEffect(() => {
-    if(auth) {
+    if (auth) {
       navigate("/");
-    }}, []);
+    }
+  }, []);
 
   useEffect(() => {
     const authData = {
-      email: localStorage.getItem('email'),
-      access: localStorage.getItem('access'),
+      email: localStorage.getItem("email"),
+      access: localStorage.getItem("access"),
     };
 
-    if(authData.access) {
+    if (authData.access) {
       dispatch(setAuth(authData));
     }
-
   }, []);
 
   return (
     <Wrapper>
-      <LoginTitle className="header">Login</LoginTitle>
-      <ErrorMessage>{newWarning}</ErrorMessage>
-      {!auth && <LoginWrapper>
-                  <form onSubmit={handleSubmit}>
-                  <FormFields>
-                    <input className="text" name="email" type="email" placeholder="Email" value={newEmail} onChange={handleChange}/>
-                  </FormFields>
-                  <FormFields>
-                    <input name="password" type="password" placeholder="Password" value={newPassword} onChange={handleChange}/>
-                  </FormFields>
-                  <button className="btn_purple" type="submit">Login</button>
-                  </form>
-                  <SignIn>
-                    New user? <Link to="/sign-up">Sign up </Link>
-                  </SignIn>
-              </LoginWrapper>
-      }
+      <div className="image_starter_page">
+        {/* RENDER BG IMAGE HERE FROM PUBLIC ASSETS FOLDER */}
+      </div>
+
+      <div className="right_container">
+
+        <LoginWrapper>
+        <h1 className="header">Login</h1>
+        <ErrorMessage>{newWarning}</ErrorMessage>
+        {!auth && (
+          <div className="form-content">
+            <form onSubmit={handleSubmit}>
+              <input
+                className="text login_input"
+                name="email"
+                type="email"
+                placeholder="Email"
+                value={newEmail}
+                onChange={handleChange}
+              />
+
+              <input
+              className="text login_input"
+                name="password"
+                type="password"
+                placeholder="Password"
+                value={newPassword}
+                onChange={handleChange}
+              />
+              <button className="btn_purple" type="submit">
+                Login
+              </button>
+            </form>
+            <div className="new_user">
+              <p>New user?</p> <Link to="/sign-up">Sign up </Link>
+            </div>
+          </div>
+        )}
+        </LoginWrapper>
+      </div>
     </Wrapper>
-
   );
-}
+};
 
-export default Login
+export default Login;
--- frontend/src/pages/Login/Login.styled.js
+++ frontend/src/pages/Login/Login.styled.js
@@ -2,73 +2,95 @@ import styled from "styled-components";
 
 export const Wrapper = styled.div`
   min-height: var(--maincontainer);
-`
+  width: 100%;
+  display: flex;
+  background-color: var(--color-orange-light);
+
+  .image_starter_page {
+    max-height: var(--maincontainer);
+    width: 50%;
+    
+    background-image: url("/assets/mynest_bg.jpg"), linear-gradient(132.96deg, #c468ff 3.32%, #ffd966 100%);
+background-blend-mode: multiply, normal;
+  background-size: cover;
+  opacity: 50%;
+
+ 
+
+  }
+
+  #mynest_bg {
+    max-height: var(--maincontainer);
+
+    background-size: contain;  
+  }
+
+  .right_container {
+    width: 50%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    padding: 1rem 2rem;
+  }
+
+  .new_user {
+    display: flex;
+    gap: 0.5rem;
+    align-items: center;
+    justify-content: center;
+  }
+  .new_user p {
+    font-size: 14px;
+    color: var(--color-grey);
+  }
+
+  .new_user a {
+    font-size: 14px;
+    color: var(--color-purple);
+    text-decoration: none;
+    cursor: pointer;
+  }
+`;
 
 export const LoginWrapper = styled.div`
-  z-index: 3;
-  height: 50%;
-  width: 35%;
+  height: 70%;
+  width: 50%;
   background-color: #fff;
   border-radius: 15px;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  padding: 1rem;
-  margin-left: auto;
-  margin-right: auto;
-  margin-bottom: 5rem;
+  border: 2px solid #7239ea;
+
+  .form-content {
+    width: 100%;
+  }
 
   form {
-    width: 55%;
+    width: 100%;
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    padding: 0px;
-
-    input:focus {
-      background-color: #f8f5ff;
-    }
-    
-    button {
-      margin-top: 15px;
-      width: 150px;
-    }
+    margin-bottom: 1rem;
   }
-`
-export const SignIn = styled.div`
-{
-  padding: 20px;
-  //background-color: #27ae60;
-}
-`
 
-export const FormFields = styled.div`
-  display: flex;
-  flex-direction: column;
-
-  input {
-    margin-bottom: 15px;
-    padding: 10px;
+  .login_input {
+    width: 70%;
+    margin: 0;
+    padding: 0.5rem;
     border: 1px solid #b5b5c3;
     border-radius: 15px;
-    width: 300px;
+    outline: none;
+    margin-bottom: 1.5rem;
   }
 
-  input:focus {
-    background-color: #f0ffff;
-    border: 1px solid #a90f1a !important;
+  .login_input:focus {
+    background-color: #f8f5ff;
   }
-`
-
-export const LoginTitle = styled.div`
-  z-index: 3;
-  width: fit-content;
-  background-color: #fff;
-  padding-bottom: 4px;
-  margin: 4rem auto 1rem;
-`
+`;
 
 export const ErrorMessage = styled.div`
   z-index: 3;
@@ -81,4 +103,4 @@ export const ErrorMessage = styled.div`
   margin-left: auto;
   margin-right: auto;
   padding: 10px;
-`
+`;
